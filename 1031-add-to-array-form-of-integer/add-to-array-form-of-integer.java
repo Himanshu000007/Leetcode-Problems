ba86c31d15ea794fce36bd185fa104195514a08a
Himanshu Kumar <151064536+Himanshu000007@users.noreply.github.com>
2026-07-07 22:20:29 +0530

    Time: 3 ms (74.27%) | Memory: 47.7 MB (49.08%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -3,16 +3,17 @@ class Solution {
         ArrayList<Integer> ans = new ArrayList<>();
         int i = num.length - 1;
 
-while (i >= 0 || k > 0) {
-    if (i >= 0) {
-        k += num[i];
-        i--;
-    }
+        while (i >= 0 || k > 0) {
+        if (i >= 0) {
+            k += num[i];
+            i--;
+        }
 
-    ans.add(k % 10);
-    k /= 10;
-}
-    Collections.reverse(ans); 
-    return ans;
+        ans.add(k % 10);
+        k /= 10;
+    }
+    
+        Collections.reverse(ans); 
+        return ans;
     }
 }
